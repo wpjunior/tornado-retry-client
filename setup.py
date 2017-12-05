565f88aa802686c60348313047db5b953dc7c4bc
Wilson Júnior <wilsonpjunior@gmail.com>
2017-12-05 14:53:53 -0200

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='tornado-retry-client',
-    version='0.4.5',
+    version='0.4.6',
     description='Simple retry tornado http client',
     long_description='',
     classifiers=[],
